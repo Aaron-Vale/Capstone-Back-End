c567272a82804e80bb164b0f4a3a63d2f68baef7
A-Aron <vale.aaron@gmail.com>
2017-11-01 13:15:04 -0400

    Add Show Script
    Add CURL script for show action on quizzes.

---
+++ scripts/quizzes/show.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TOKEN="BAhJIiU1YjVlNDhlZjU2NmIxYmU2YjFiNDllNDZkYmJlNDhiZQY6BkVG--7305328323cc6e3d9337e374123003b2dfadeb3f"
+API="${API_ORIGIN:-http://localhost:4741}" \
+URL_PATH="/quizzes/${ID}"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=$TOKEN"
+
+echo
