498241b452af21fa61c096989c562f2ebf1c9ab1
A-Aron <vale.aaron@gmail.com>
2017-11-02 14:52:57 -0400

    Add Content Editable for Quiz Serializer
    Add editable function which sends a boolean indicating whether or not
    the currently logged-in user can edit a quiz.

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,4 +1,8 @@
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :category, :title, :user, :questions
+  attributes :id, :category, :title, :user, :questions, :editable
   has_one :user
+
+  def editable
+    scope == object.user
+  end
 end
